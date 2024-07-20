476aa68080f4b57d9682bc355762efad4c5e837e
ZachMcLean <zmclean11@gmail.com>
2024-07-20 08:13:06 -0500

    setup for project 3

---
+++ python-projects/project3/minesweeper.py
@@ -0,0 +1,11 @@
+# Zach McLean
+# CSCI 3038
+
+# Minesweeper in python
+
+# Your task is to write a Python program that will allow someone to play a 
+# text-based Minesweeper game in the terminal. Your program only needs to 
+# implement a basic 5x5 board with 4 mines randomly placed.It should be fully 
+# playable and include flagging as a feature. After winning or losing, your program should
+# display the entire board uncovered and write out, to a separate text file, that full board uncovered, 
+# and the list of moves made by the user.
